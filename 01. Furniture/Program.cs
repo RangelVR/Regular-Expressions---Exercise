b6fc71ea8f1a6bdbb4627a10bad84a3a91d80da0
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-18 18:51:02 +0200

    Add files via upload

---
+++ 01. Furniture/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+Regex regex = new Regex(@">>(?<name>[A-Za-z]+)<<(?<price>\d+(\.\d*)?)!(?<quantity>\d+)");
+
+decimal totalSum = 0;
+
+while (input != "Purchase")
+{
+    Match match = regex.Match(input);
+
+    if (match.Success)
+    {
+        string name = match.Groups["name"].Value;
+        decimal price = decimal.Parse(match.Groups["price"].Value);
+        int quantity = int.Parse(match.Groups["quantity"].Value);
+
+        sb.AppendLine(name);
+        totalSum += price * quantity;
+    }
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Bought furniture:");
+Console.Write(sb);
+Console.WriteLine($"Total money spend: {totalSum:f2}");
