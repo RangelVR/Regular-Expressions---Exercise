f79cf20dec2b23dfeb0d21de2b41ebfd93bd85bb
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-18 20:45:22 +0200

    Add files via upload

---
+++ 02. Race/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Text.RegularExpressions;
+
+string[] inputNames = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+string info = Console.ReadLine();
+
+var participants = new Dictionary<string, int>();
+
+foreach (var name in inputNames)
+{
+    participants.Add(name, 0);
+}
+
+while (info != "end of race")
+{
+    MatchCollection name = Regex.Matches(info, "[A-Za-z]+");
+    MatchCollection distanceRegxColection = Regex.Matches(info, @"\d");
+
+    string curName = string.Join("", name);
+
+    if (participants.ContainsKey(curName))
+    {
+        participants[curName] += distanceRegxColection.Select(x => int.Parse(x.Value)).Sum();
+    }
+
+    info = Console.ReadLine();
+}
+
+var winners = participants.OrderByDescending(x => x.Value).Take(3).ToList();
+
+int counter = 1;
+foreach (var participant in winners)
+{
+    string suffix = counter == 1 ? "st" : counter == 2 ? "nd" : "rd"; 
+    Console.WriteLine($"{counter++}{suffix} place: {participant.Key}");
+}
